d19cc40630a56b3ca829af2bd4d379751219a98f
Admin <Admin@MacBook-Pro-2.local>
2019-07-31 17:30:55 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+
+
+$('#addSkill').on('click', function() {
+    input = document.querySelector('input').value;
+    let $li = $(`<li><button>X</button>${input}</li>`);
+   
+
+    $('ul').append($li);
+    $('input').val("");
+});
+
+$('ul button').on('click', 'button', function() {
+    $(this).closest('li').remove();
+})
